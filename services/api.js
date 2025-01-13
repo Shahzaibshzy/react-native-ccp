e61e6d563289df3bed3e7d93504cf13a1bc83e64
Shahzaib Khan <112001025+Shahzaibshzy@users.noreply.github.com>
2025-01-13 15:07:23 +0500

    Update api.js

--- services/api.js
+++ services/api.js
@@ -4,11 +4,6 @@ import mockData from './mockData.json';
 // Simulate a Mock API
 export const fetchTeacherData = async () => {
   try {
-    // Uncomment if using a real API
-    // const response = await axios.get('https://example.com/api');
-    // return response.data;
-
-    // Simulate an API delay
     await new Promise((resolve) => setTimeout(resolve, 1000));
     return mockData;
   } catch (error) {
